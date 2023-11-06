833ee6e10876f637940f5268fab914f4238b64c3
Muhammad Dujana Aziz <79629886+Dujana-aziz@users.noreply.github.com>
2023-11-06 15:33:11 +0500

    connected to mongoDB atlas

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Connecting the data base
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to DB')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 
 const app = express()
 
