7a547b833900495fec4cfd667ca7bae221bf784d
Muhammad Dujana Aziz <79629886+Dujana-aziz@users.noreply.github.com>
2023-11-07 13:27:46 +0500

    complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -19,17 +19,20 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body
   try {
+    // Find user in the db
     const validUser = await User.findOne({ email })
     if (!validUser) return next(errorhandler(404, 'User not found!'))
 
+    // validate user password
     const validPassword = bcryptjs.compareSync(password, validUser.password)
     if (!validPassword) return next(errorhandler(401, 'Wrong Credentials!'))
 
+    // Change that user into a token using jwt
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
     // removing password from the post response
     const { password: pass, ...rest } = validUser._doc
+    // send the response of the valid user in cookies while tokenizing it
     res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
-
     // res.cookie('access_token',token,{httpOnly:true, expires: new Date(Date.now() + 24 * 60 * 60 * 1000 )})
   } catch (error) {
     next(error)
