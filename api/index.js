00d41b5d5fe21d27e076346f6fce416847de6505
Muhammad Dujana Aziz <79629886+Dujana-aziz@users.noreply.github.com>
2023-11-20 15:49:29 +0500

    changed index.js

--- api/index.js
+++ api/index.js
@@ -1,29 +1,26 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import cookieParser from 'cookie-parser'
-
+import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
+import cookieParser from 'cookie-parser'
 import path from 'path'
-
-import dotenv from 'dotenv'
 dotenv.config()
 
-// Connecting the data base
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('Connected to DB')
+    console.log('Connected to MongoDB!')
   })
-  .catch((error) => {
-    console.log(error)
+  .catch((err) => {
+    console.log(err)
   })
 
 const __dirname = path.resolve()
 
-// Connecting with backend server using express on port 3000
 const app = express()
+
 app.use(express.json())
 
 app.use(cookieParser())
@@ -33,17 +30,16 @@ app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!')
 })
 
-// Routes to communicate between Client & Server
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
 
 app.use(express.static(path.join(__dirname, '/client/dist')))
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
 
-// Middle Ware for handling server db error
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500
   const message = err.message || 'Internal Server Error'
